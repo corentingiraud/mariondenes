1a0a4ab85d1a1c45e868e5e6e1d3369762593ef4
Corentin GIRAUD <corentin.giraud@insa-lyon.fr>
2018-09-07 10:47:49 -0400

    Update about-me

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -7,6 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  monParcours = [
+    {
+      date: '2007-2011',
+      description: ['Brevet des collèges Classe audio visuelle'],
+      descriptionMobile: ['Brevet des collèges', 'Classe audio visuelle'],
+    },
+    {
+      date: '2011-2014',
+      description: ['BAC série L, anglais renforcé, option théâtre'],
+      descriptionMobile: ['BAC série L, anglais', 'renforcé, option théâtre'],
+    },
+    {
+      date: '2014-2017',
+      description: ['Bachelor design graphique, Option médias imprimés'],
+      descriptionMobile: ['Bachelor design graphique', 'Option médias imprimés'],
+    },
+    {
+      date: '2017-2018',
+      description: ['Service civique'],
+      descriptionMobile: ['Service civique'],
+    },
+  ];
+
+  mesCompetences = [
+    {
+      pictoName: 'camera.png',
+      title: 'Photographie',
+    },
+    {
+      pictoName: 'camera.png',
+      title: 'Impression artisanale',
+    },
+    {
+      pictoName: 'camera.png',
+      title: 'PAO',
+    },
+    {
+      pictoName: 'camera.png',
+      title: 'Animation',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
