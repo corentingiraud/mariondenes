3eb3858ef964a4352e3d5fac75a00b0c33635446
corentingiraud <corentin.giraud@insa-lyon.fr>
2018-12-20 14:46:57 -0500

    Update dependencies + remove menu anim

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,39 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
-import { WindowScrolling } from '../_services/windows.service';
 import { ObservableMedia, MediaChange } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  animations: [
-    trigger('fadeIn', [
-      state('active-desktop', style({opacity: 1})),
-      state('active-mobile', style({opacity: 1})),
-      state('inactive', style([{opacity: 0}, {'z-index': -1}])),
-      state('none', style([{opacity: 0}, {'z-index': -1}])),
-      transition('inactive => active-mobile', animate(80, keyframes([
-        style({opacity: 0, offset: 0}),
-        style({opacity: 1, offset: 1}),
-      ]))),
-      transition('none => active-mobile', animate(80, keyframes([
-        style({opacity: 0, offset: 0}),
-        style({opacity: 1, offset: 1}),
-      ]))),
-      transition('active-mobile => inactive', animate(80, keyframes([
-        style({opacity: 1, offset: 0}),
-        style({opacity: 0, offset: 1}),
-      ]))),
-    ])
-  ]
 })
 export class MenuComponent implements OnInit {
 
   isActive: Boolean = false;
   fadeInState: string;
 
-  constructor(private windowScrolling: WindowScrolling, private media: ObservableMedia) {
+  constructor(private media: ObservableMedia) {
   }
 
   ngOnInit() {
@@ -42,11 +20,9 @@ export class MenuComponent implements OnInit {
     this.media.subscribe((change: MediaChange) => {
       if (change.mqAlias === 'xs' || change.mqAlias === 'sm' ) {
         this.isActive = false;
-        this.fadeInState = 'none';
       }
       if (change.mqAlias === 'md') {
         this.isActive = true;
-        this.fadeInState = 'active-desktop';
       }
     });
   }
@@ -54,7 +30,6 @@ export class MenuComponent implements OnInit {
   toogle() {
     if (this.media.isActive('xs') || this.media.isActive('sm')) {
       this.isActive = !this.isActive;
-      this.fadeInState = this.isActive ? 'active-mobile' : 'inactive';
     }
   }
 }
